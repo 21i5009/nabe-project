4ac3086b84af7c6c5e637b01f19d9a5012cffcae
21i5009 <153479954+21i5009@users.noreply.github.com>
2023-12-11 13:40:58 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.AB, function () {
+    ナベアツの素 = randint(1, 9)
+    _3の倍数 = 3
+    if (ナベアツの素 == _3の倍数) {
+        music.play(music.stringPlayable("- C D F G - - - ", 120), music.PlaybackMode.InBackground)
+        basic.showNumber(ナベアツの素)
+        basic.showIcon(IconNames.Confused)
+    } else if (false) {
+    	
+    } else {
+        music.play(music.stringPlayable("- G F D C - - - ", 120), music.PlaybackMode.InBackground)
+        basic.showNumber(ナベアツの素)
+        basic.showIcon(IconNames.Happy)
+    }
+})
+let _3の倍数 = 0
+let ナベアツの素 = 0
+basic.showString("NABEATU")
